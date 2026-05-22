e99dbba1b21aacd5d91db84610eb9060cceb1c4d
agregorka <agregorka@gmail.com>
2026-05-22 15:05:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
+import pandas
 
 # Write directly to the app
 st.title(f"\U0001F964 Customize Your Smoothie \U0001F964")
